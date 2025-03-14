74683d5ca9d079823e813fe64e973f2e5254d6de
Ákos Lovász <akos.lovasz@chameleon.sh>
2025-03-14 13:28:31 +0100

    remove reliance on load times, smoother load

--- f1tv-spoilers.user.js
+++ f1tv-spoilers.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name             Spoiler Free Mode for F1TV
-// @version          1.0
+// @version          1.1
 // @downloadURL      https://github.com/Lovasz-Akos/F1TV-Spoiler-Free-Mode/raw/refs/heads/main/f1tv-spoilers.user.js
 // @updateURL        https://github.com/Lovasz-Akos/F1TV-Spoiler-Free-Mode/raw/refs/heads/main/f1tv-spoilers.user.js
 // @description      Remove all banners and thumbnail containers, only show sessions
@@ -12,8 +12,6 @@
 // ==/UserScript==
 
 window.onload = async function () {
-  const endTime = Date.now() + 2000;
-
   while (document.getElementsByClassName("super-hero container")[0] == null) {
     await new Promise((r) => setTimeout(r, 1));
   }
@@ -23,7 +21,7 @@ window.onload = async function () {
 
   const sessionDiv = document.getElementsByClassName("container")[0];
 
-  while (Date.now() < endTime) {
+  while (document.getElementsByClassName("home-page")[0].childElementCount > 1) {
     var spoilerBanners = document.getElementsByClassName("container");
     for (let index = 0; index < spoilerBanners.length; index++) {
       if (index >= 3 && spoilerBanners[index] != sessionDiv) {
