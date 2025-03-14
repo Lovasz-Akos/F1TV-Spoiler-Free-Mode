f530b049876c9412b7b1c14520dae60f7554fc36
Ákos Lovász <akos.lovasz@chameleon.sh>
2025-03-14 13:21:02 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # F1TV-Spoiler-Free-Mode
+
 A bruteforce way to make F1TV Spoiler free, only shows the sessions to watch
+
+No superhero, no news, no highlights, just pure session picker
+
+![F1TV Spoiler Free Mode Preview](preview.png)
+
+## Issues
+
+Currently I can't get a good handle when navigating by the top bar, so it can only remove spoiler when reloading or opening the page for the first time.
---
+++ f1tv-spoilers.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name             Spoiler Free Mode for F1TV
+// @version          1.0
+// @downloadURL      https://github.com/Lovasz-Akos/F1TV-Spoiler-Free-Mode/raw/master/f1tv-spoilers.user.js
+// @updateURL        https://github.com/Lovasz-Akos/F1TV-Spoiler-Free-Mode/raw/master/f1tv-spoilers.user.js
+// @description      Remove all banners and thumbnail containers, only show sessions
+// @author           https://github.com/Lovasz-Akos
+// @match            https://f1tv.formula1.com/
+// @icon             https://www.google.com/s2/favicons?sz=64&domain=formula1.com
+// @grant            none
+// @run-at           document-end
+// ==/UserScript==
+
+window.onload = async function () {
+  const endTime = Date.now() + 2000;
+
+  while (document.getElementsByClassName("super-hero container")[0] == null) {
+    await new Promise((r) => setTimeout(r, 1));
+  }
+
+  const bigBanner = document.getElementsByClassName("super-hero container");
+  bigBanner[0].remove();
+
+  const sessionDiv = document.getElementsByClassName("container")[0];
+
+  while (Date.now() < endTime) {
+    var spoilerBanners = document.getElementsByClassName("container");
+    for (let index = 0; index < spoilerBanners.length; index++) {
+      if (index >= 3 && spoilerBanners[index] != sessionDiv) {
+        spoilerBanners[index].remove();
+      }
+    }
+  }
+
+  console.log("Finished removing spoilers");
+};
